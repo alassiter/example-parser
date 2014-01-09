caa5f132a89f7aecac495a2f8edfce194d0432f0
Anthony Lassiter <anthonylassiter@gmail.com>
2014-01-09 09:52:15 -0600

    Add parser and parser_spec
    I added these files after creating several tests and methods. Going
    forward, these commit messages should convey a better sense of the
    work I'm doing.

---
+++ parser.rb
@@ -0,0 +1,58 @@
+class Parser
+  attr_reader :original_string
+  attr_accessor :data
+
+  def initialize(file)
+    @original_string = File.read(file)
+    @data = {}
+  end
+
+  def headers
+    headers = []
+    @original_string.each_line do |line|
+      if header?(line)
+        headers << extract_header(line)
+      end
+    end
+    headers
+  end
+
+  def create_data(string)
+    current_header = ''
+    string.each_line do |line|
+      if header?(line)
+        current_header = extract_header(line)
+        @data[current_header] = {}
+      end
+    end
+    @data
+  end
+
+  def header?(string)
+    !!(string =~ header_match)
+  end
+
+  def key_value?(string)
+    !!(string =~ key_value_match)
+  end
+
+  def extract_header(string)
+    string.match(header_match)[1]
+  end
+
+  def extract_key_value(string)
+    key_value_array = string.split(":").map!{|e| e.strip}
+    Hash[*key_value_array]
+  end
+
+  private
+  def header_match
+    /^\[\W*([a-zA-Z0-9 ]+\b)\W*\]/
+    # /^\[\W*(.+)\W*\]/
+  end
+
+  def key_value_match
+    /^\w+\s*:\s*\w+\s*/
+  end
+
+end
---
+++ spec/parser_spec.rb
@@ -0,0 +1,108 @@
+require 'spec_helper'
+
+describe Parser do
+  before :all do
+    @parser = Parser.new("config.txt")
+  end
+
+  describe "initialize" do
+    it "reads the config file" do
+      @parser.original_string.should eql(File.read("config.txt"))
+    end
+
+    it "has a data attribute" do
+      @parser.data.should be_a(Hash)
+    end
+  end
+
+  describe "#header?" do
+    it "recognizes a standard header" do
+      string = "[header]\n"
+      @parser.header?(string).should be(true)
+    end
+
+    it "recognizes a header with surrounding spaces" do
+      string = "[ header ]\n"
+      @parser.header?(string).should be(true)
+    end
+
+    it "recognizes a header with spaces in name" do
+      string = "[meta data]\n"
+      @parser.header?(string).should be(true)
+    end
+
+    it "does not recognize a header with a space in column1" do
+      string = " [header]\n"
+      @parser.header?(string).should_not be(true)
+    end
+
+    it "does not recognize a key:value as a header" do
+      string = "key:value\n"
+      @parser.header?(string).should_not be(true)
+    end
+  end
+
+  describe "#extract_header" do
+    it "returns header from a standard header" do
+      string = "[header]\n"
+      @parser.extract_header(string).should eql("header")
+    end
+
+    it "returns header from a header with spaces" do
+      string = "[ header ]\n"
+      @parser.extract_header(string).should eql("header")
+    end
+  end
+
+  describe "#key_value?" do
+    it "recognizes a standard key value" do
+      string = "key:value\n"
+      @parser.key_value?(string).should be(true)
+    end
+
+    it "recognizes a key value with spaces around colon" do
+      string = "key :  value \n"
+      @parser.key_value?(string).should be(true)
+    end
+
+    it "does not recognize a key value with a space in column 1" do
+      string = " key:value\n"
+      @parser.key_value?(string).should be(false)
+    end
+  end
+
+  describe "#extract_key_value" do
+    it "returns a hash" do
+      string = "key:value\n"
+      @parser.extract_key_value(string).should be_a(Hash)
+    end
+
+    it "returns a key value from a standard key value" do
+      string = "key:value\n"
+      @parser.extract_key_value(string).should eql({'key' => 'value'})
+    end
+  end
+
+  describe "#headers" do
+    it "returns an array of headers" do
+      array = ["header", "meta data", "trailer"]
+      @parser.headers.should =~ array
+    end
+  end
+
+  describe "create_data" do
+    it "contains headers on first level" do
+      headers = ["header1", "header2"]
+      string = "[header1]\n[header2]\n"
+      @parser.create_data(string)
+      @parser.data.keys.should =~ headers
+    end
+
+    it "contains a section with header and key value" do
+      string = "[header1 ]\napple: red\n"
+      @parser.create_data(string)
+      @parser.data.should eql( {'header1' => {'apple' => 'red'}} )
+    end
+  end
+
+end
